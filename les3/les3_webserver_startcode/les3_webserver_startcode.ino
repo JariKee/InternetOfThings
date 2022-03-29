d968201b2afa86541919226142bdc5af17bb3f1a
33975 <jarip99q@gmail.com>
2022-03-29 11:53:22 +0100

    .

--- les3/les3_webserver_startcode/les3_webserver_startcode.ino
+++ les3/les3_webserver_startcode/les3_webserver_startcode.ino
@@ -21,7 +21,7 @@ bool led2Status = false; //toggle led on/off
 bool led3Status = false; //toggle led on/off
 
 // your data
-String studentName = "Jacob student nummer 32894"; // jouw student nummer + naam
+String studentName = "Jari Kee 33975"; // jouw student nummer + naam
 
 const char* ssid = "Medialab";
 const char* password = "Mediacollege2";
